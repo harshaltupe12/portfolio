b15d3c6e5b2f999547367efc3e87b44836d2dab1
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-12 19:52:48 +0530

    Doing Minor changes in home page

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -4,11 +4,13 @@ import { SparklesCore } from "../../components/ui/sparkles";
 import { ArrowRight } from "lucide-react";
 import { FlipWords } from "../../components/ui/flip-words";
 import { color } from "framer-motion";
+import { FileDown } from 'lucide-react';
+import WhatIDo from "../info/page";
 
 function Home() {
   const words = ["Software-Developer", "Fullstack-Developer", "REST-API"];
   return (
-    <div className="h-screen relative w-full bg-[#fff] flex flex-col items-center md:justify-center justify-start overflow-hidden rounded-md">
+    <div className="max-h-min md:h-screen relative w-full bg-[#fff] flex flex-col items-center md:justify-center justify-start overflow-hidden rounded-md">
       <div className="w-full absolute inset-0 h-screen">
         <SparklesCore
           id="tsparticlesfullpage"
@@ -21,7 +23,7 @@ function Home() {
         />
       </div>
       <div className="flex justify-center flex-col items-center z-10 mt-10 md:mt-0">
-        <div className="bg-slate-200 w-[max-content] p-2 rounded-lg flex gap-1 font-bold text-gray-700 cursor-pointer hover:scale-105 transition-all">
+        <div className="bg-gradient-to-tl from-blue-400 via-blue-100 to-blue-400 w-[max-content] p-2 rounded-lg flex gap-1 font-bold text-gray-700 cursor-pointer hover:scale-105 transition-all">
           Do check GitHub <ArrowRight />
         </div>
         <h2 className=" my-2 text-[40px] md:text-[70px] font-bold text-gray-900 flex flex-col md:flex-row md:gap-2 gap-0 items-center">
@@ -41,8 +43,8 @@ function Home() {
             <br />
           </div>
         </div>
-        <div className="md:mt-8 my-4 bg-slate-200 ">
-          <span className="bg-gradient-to-tl from-blue-400 via-pink-400 to-violet-400 text-slate-100 md:text-[20px] text-[15px] p-2 rounded-lg">
+        <div className="md:mt-8 my-4 bg-slate-200 cursor-pointer hover:scale-105 transition-all ">
+          <span className="bg-gradient-to-tl from-violet-400 via-pink-300 to-violet-400 text-gray-900 md:text-[15px] text-[15px] p-2 rounded-lg font-bold">
             {" "}
             Download Resume
           </span>
