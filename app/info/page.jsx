f74cc7daa7b763007edceae57dcd684791693667
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-13 17:05:25 +0530

    Link Chnage

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -8,25 +8,25 @@ function WhatIDo() {
       title: "Front-end Developer",
       description:
         "I specialize in crafting engaging and responsive user interfaces using modern frontend technologies like React, JavaScript, and Tailwind CSS. My expertise lies in creating seamless user experiences and visually appealing web applications.",
-      link: "#",
+      link: "/",
     },
     {
       title: "Back-end Developer",
       description:
         "I excel in developing robust and scalable backend systems using Node.js, Express, and MongoDB, PostgreSQL. My focus is on building efficient APIs and managing data storage to support seamless and performant web applications.",
-      link: "#",
+      link: "/",
     },
     {
       title: "Responsive Design",
       description:
         "I am adept at creating responsive web designs that ensure optimal user experiences across all devices and screen sizes. Utilizing CSS frameworks and media queries, I build layouts that adapt fluidly and maintain visual integrity.",
-      link: "#",
+      link: "/",
     },
     {
       title: "UI/UX Designer",
       description:
         "As a UI/UX designer, I specialize in crafting intuitive and visually appealing interfaces that enhance user satisfaction. I focus on user-centered design principles to create engaging and seamless experiences across digital products.",
-      link: "#",
+      link: "/",
     },
     
   ];
