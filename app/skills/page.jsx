102efd9f18406a6695c73cf2046e0af86e155211
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-13 17:06:07 +0530

    Created Skill section

--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -5,14 +5,18 @@ import { skillsData } from "@/utils/skills";
 import { skillsImage } from "@/utils/skill-image";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
-
+// bg-[#11152c]
 function Skills() {
   return (
-    <div>
-      <div
-        id="skills" className="relative z-50 my-12 lg:my-24">
+    <div className="md:mx-8 mx-4">
+      <div id="skills" className="relative z-50 my-2 ">
         <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
-
+        <h2 className="mb-4">
+          <span className="md:text-4xl text-3xl ml-2 md:ml-4">Technical {" "}</span> 
+          <span className="bg-clip-text bg-gradient-to-tl from-blue-600 via-red-400 to-violet-600 text-transparent md:text-4xl text-3xl font-bold">
+            Skills
+          </span>
+        </h2>
         <div className="w-full my-1">
           <Marquee
             gradient={false}
@@ -28,7 +32,7 @@ function Skills() {
                 className="w-36 min-w-fit h-fit flex flex-col items-center justify-center transition-all duration-500 m-3 sm:m-5 rounded-lg group relative hover:scale-[1.15] cursor-pointer"
                 key={id}
               >
-                <div className="h-full w-full rounded-lg border border-[#1f223c] bg-[#11152c] shadow-none shadow-gray-50 group-hover:border-violet-500 transition-all duration-500">
+                <div className="h-full w-full rounded-lg border border-[#1f223c] bg-[#ffffff] shadow-none shadow-gray-50 group-hover:border-violet-500 transition-all duration-500">
                   <div className="flex -translate-y-[1px] justify-center">
                     <div className="w-3/4">
                       <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-violet-500 to-transparent" />
@@ -44,7 +48,7 @@ function Skills() {
                         className="h-full w-auto rounded-lg"
                       />
                     </div>
-                    <p className="text-white text-sm sm:text-lg">{skill}</p>
+                    <p className="text-black text-sm sm:text-lg">{skill}</p>
                   </div>
                 </div>
               </div>
