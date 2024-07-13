0c2e4656a05850a1adbd19b69850bdcf1b18e419
Harshal Tupe <harshaltupe12@gmail.com>
2024-07-13 17:06:36 +0530

    Added Acertinity UI Components

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -25,8 +25,8 @@ export const HoverEffect = ({
     >
       {items.map((item, idx) => (
         <Link
-          href={item?.link}
-          key={item?.link}
+          href={""}
+          key={idx}
           className="relative group  block p-2 h-full w-full"
           onMouseEnter={() => setHoveredIndex(idx)}
           onMouseLeave={() => setHoveredIndex(null)}
