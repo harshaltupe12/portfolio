144bff1259e061eec435040642dca7cd77dcdb26
Harshal Tupe <harshaltupe12@gmail.com>
2025-03-01 20:48:02 +0530

    Packages and Configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:['assets.aceternity.com'],
+        // unoptimized:true
+    }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,40 +6,179 @@ const {
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      animation: {
-        "meteor-effect": "meteor 5s linear infinite",
-        scroll:"scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
-      },
-      keyframes: {
-        scroll: {
-          to: {
-            transform: "translate(calc(-50% - 0.5rem))",
-          },
-        },
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
-          },
-        },
-      },
-    },
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		animation: {
+  			'meteor-effect': 'meteor 5s linear infinite',
+  			scroll: 'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
+  			shine: 'shine var(--duration) infinite linear'
+  		},
+  		keyframes: {
+  			scroll: {
+  				to: {
+  					transform: 'translate(calc(-50% - 0.5rem))'
+  				}
+  			},
+  			meteor: {
+  				'0%': {
+  					transform: 'rotate(215deg) translateX(0)',
+  					opacity: '1'
+  				},
+  				'70%': {
+  					opacity: '1'
+  				},
+  				'100%': {
+  					transform: 'rotate(215deg) translateX(-500px)',
+  					opacity: '0'
+  				}
+  			},
+  			shine: {
+  				'0%': {
+  					'background-position': '0% 0%'
+  				},
+  				'50%': {
+  					'background-position': '100% 100%'
+  				},
+  				to: {
+  					'background-position': '0% 0%'
+  				}
+  			},
+  			'aurora-border': {
+  				'0%, 100%': {
+  					borderRadius: '37% 29% 27% 27% / 28% 25% 41% 37%'
+  				},
+  				'25%': {
+  					borderRadius: '47% 29% 39% 49% / 61% 19% 66% 26%'
+  				},
+  				'50%': {
+  					borderRadius: '57% 23% 47% 72% / 63% 17% 66% 33%'
+  				},
+  				'75%': {
+  					borderRadius: '28% 49% 29% 100% / 93% 20% 64% 25%'
+  				}
+  			},
+  			'aurora-1': {
+  				'0%, 100%': {
+  					top: '0',
+  					right: '0'
+  				},
+  				'50%': {
+  					top: '50%',
+  					right: '25%'
+  				},
+  				'75%': {
+  					top: '25%',
+  					right: '50%'
+  				}
+  			},
+  			'aurora-2': {
+  				'0%, 100%': {
+  					top: '0',
+  					left: '0'
+  				},
+  				'60%': {
+  					top: '75%',
+  					left: '25%'
+  				},
+  				'85%': {
+  					top: '50%',
+  					left: '50%'
+  				}
+  			},
+  			'aurora-3': {
+  				'0%, 100%': {
+  					bottom: '0',
+  					left: '0'
+  				},
+  				'40%': {
+  					bottom: '50%',
+  					left: '25%'
+  				},
+  				'65%': {
+  					bottom: '25%',
+  					left: '50%'
+  				}
+  			},
+  			'aurora-4': {
+  				'0%, 100%': {
+  					bottom: '0',
+  					right: '0'
+  				},
+  				'50%': {
+  					bottom: '25%',
+  					right: '40%'
+  				},
+  				'90%': {
+  					bottom: '50%',
+  					right: '25%'
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
+  			'color-1': 'hsl(var(--color-1))',
+  			'color-2': 'hsl(var(--color-2))',
+  			'color-3': 'hsl(var(--color-3))',
+  			'color-4': 'hsl(var(--color-4))',
+  			'color-5': 'hsl(var(--color-5))'
+  		}
+  	}
   },
-  plugins: [addVariablesForColors],
+  plugins: [addVariablesForColors, require("tailwindcss-animate")],
 };
 export default config;
 
